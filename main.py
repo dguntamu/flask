d85b3febf2943476f5cd494d189b8d1e2a17f063
Dhananjaya Rao G <meet.dhanuhyd@gmail.com>
2025-07-31 10:04:48 +0530

    Integration HTML with FLASK and HTTP verb GET and POST

---
+++ main.py
@@ -0,0 +1,58 @@
+## Integration HTML with FLASK
+## HTTP verb GET and POST
+
+from flask import Flask,redirect,url_for,render_template,request
+
+
+#Web Server Gateway Interface
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return render_template('index.html');
+
+@app.route('/success/<int:score>')
+def success(score):
+    final_result = ''
+    if score >= 50:
+        final_result = 'PASS'
+    else:
+        final_result = "FAIL"
+    return render_template('result.html',res = final_result);
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "Failed with score :: "+str(score);
+
+''' Building URL dynamically '''
+@app.route('/result/<int:marks>')
+def result(marks):
+    status = ''
+
+    if marks < 35:
+        status = 'fail'
+    else:
+        status = 'success'
+
+    return redirect(url_for(status,score=marks))
+
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    if request.method == 'POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        datascience = float(request.form['datascience'])
+
+        avg = (science+maths+c+datascience)/4
+        if avg >= 50:
+            result = 'success'
+        else:
+            result = 'fail'
+
+        return redirect(url_for(result,score = avg))
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
