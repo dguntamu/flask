728e6467826685d09a256ab69076a929b974d2f1
Dhananjaya Rao G <meet.dhanuhyd@gmail.com>
2025-07-31 08:39:32 +0530

    Flask - Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,redirect,url_for
+#Web Server Gateway Interface
+app = Flask(__name__)
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "Passed with score : "+ str(score);
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "Failed with score :: "+str(score);
+
+@app.route('/result/<int:marks>')
+def result(marks):
+    status = ''
+
+    if marks < 35:
+        status = 'fail'
+    else:
+        status = 'success'
+
+    return redirect(url_for(status,score=marks))
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
